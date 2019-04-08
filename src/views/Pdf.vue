ce22ea851f5170452c12167d9a6ca0a70a75353d
unknown <hungry4h@nate.com>
2019-04-08 22:10:23 +0900

    second pdf

--- src/views/Pdf.vue
+++ src/views/Pdf.vue
@@ -1,42 +1,13 @@
 <template>
-	<div>
-        <v-container>
-		<pdf
-			v-for="i in numPages"
-			:key="i"
-			:src="src"
-			:page="i"
-			style="display: inline-block; width: 100%"
-		></pdf>
-        </v-container>
-	</div>
+  <pdf src="/pdf.pdf"></pdf>
 </template>
 
 <script>
-
-//https://github.com/FranckFreiburger/vue-pdf
-
 import pdf from 'vue-pdf'
 
-var loadingTask = pdf.createLoadingTask('/pdf.pdf');
-
 export default {
-	components: {
-		pdf
-	},
-	data() {
-		return {
-			src: loadingTask,
-			numPages: undefined,
-		}
-	},
-	mounted() {
-
-		this.src.then(pdf => {
-
-			this.numPages = pdf.numPages;
-		});
-	}
+  components: {
+    pdf
+  }
 }
-
 </script>
